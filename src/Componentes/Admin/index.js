b4df8cb6d5768da1d7e5b0b4312b1348a2e6c393
Carlos Moya <carlosmoya@gmail.com>
2020-04-13 16:13:36 -0300

    agrega precampo para Departamento/rol

--- src/Componentes/Admin/index.js
+++ src/Componentes/Admin/index.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { withFirebase } from '../Firebase'
-import {firebase} from '../Firebase/firebase'
-import {AuthUserContext ,withAuthorization} from '../Session'
+import { firebase } from '../Firebase/firebase'
+import { AuthUserContext, withAuthorization } from '../Session'
 
 
 function useObtenerUsuarios() {
   const [loading, setLoading] = useState(false)
   const [users, setUsers] = useState([])
-  
+  const [tareas, setTareas] = useState([])
+
 
   useEffect(() => {
     setLoading(true)
 
-    firebase.firestore().collection('usuarios').onSnapshot(
+    const unsuscribe = firebase.firestore().collection('usuarios').onSnapshot(
       snapshot => {
         const nuevosEmpleados = snapshot.docs.map(
           doc => ({
@@ -24,34 +25,54 @@ function useObtenerUsuarios() {
         console.log(users)
       }
     )
+
+    const unsuscribeTo = firebase.firestore().collection('tareas').onSnapshot(
+      snapshot => {
+        const nuevasTareas = snapshot.docs.map(
+          doc => ({
+            id: doc.id,
+            ...doc.data()
+          })
+        )
+        setTareas(nuevasTareas)
+      }
+    )
+    return () => [unsuscribe(), unsuscribeTo()]
   }, [])
-  return users
+  return [users, tareas]
 }
 
 const Admin = props => {
-
-  const users = useObtenerUsuarios(props)
+  const [users, tareas] = useObtenerUsuarios()
   console.log(users)
+  console.log(tareas)
+
   return (
     <div>
       <h1>Administrador</h1>
       <ul>
-        {users.map(user =>(
+        {users.map(user => (
           <li key={user.id}>
-            <h5>Email: {user.nombre}</h5>
-            <h5>UserName: {user.apellido}</h5>
+            <h5>Email: {user.email}</h5>
+            <h5>UserName: {user.userName}</h5>
+            <h6>Departamento: {user.departamento}</h6>
           </li>
         ))}
       </ul>
 
       <AuthUserContext.Consumer>
         {authUser => {
+          let departamento = ''
+          users.map( user => (user.email === authUser.email)? departamento = user.departamento: null)
           return (
             <div>
-              <h3>{authUser.email}</h3>
-            </div>
-          )
-        }}
+              {tareas.map(tarea => {
+                return (tarea.departamento === departamento 
+                  ? <h4 key={tarea.id}>Mostrando tareas del departamento: {tarea.departamento}</h4> 
+                  : null
+                  )})}
+              </div>
+            )}}
       </AuthUserContext.Consumer>
     </div>
   )
--- src/Componentes/SignUp/SignUpForm.js
+++ src/Componentes/SignUp/SignUpForm.js
@@ -3,6 +3,8 @@ import { withFirebase } from '../Firebase/index'
 import { withRouter } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes';
 import { compose } from 'recompose'
+import { firebase } from '../Firebase/firebase'
+import { firestore } from 'firebase';
 
 
 const SignUpFormBase = (props) => {
@@ -11,69 +13,78 @@ const SignUpFormBase = (props) => {
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
   const [error, setError] = useState(null)
+  const [departamento, setDepartamento] = useState('')
 
 
   const onSubmit = (e) => {
     props.firebase
-    .doCreateUserWithEmailAndPassword(email, password1)
-    .then((authUser) => {
-      return props.firebase
-      .user(authUser.user.uid)
-      .set({
-        userName,
-        email,
-      })
-    }
+      .doCreateUserWithEmailAndPassword(email, password1)
+      .then(() => {
+        firebase.firestore().collection('usuarios').add({
+          email,
+          userName,
+          departamento,
+        })
+      }
 
-    )
-    .then(()=>{
-      setUserName('');
-      setEmail('');
-      setPassword1('');
-      setPassword2('');
-      props.history.push(ROUTES.ENCUESTA)
-    })
-    .catch(error => setError(error));
+      )
+      .then(() => {
+        setUserName('')
+        setEmail('')
+        setPassword1('')
+        setPassword2('')
+        setDepartamento('')
+        props.history.push(ROUTES.ENCUESTA)
+      })
+      .catch(error => setError(error));
 
     e.preventDefault()
   }
 
   const isInvalid = password1 !== password2 ||
-              userName === '' || 
-              password1 === '' ||
-              email === '';
+    userName === '' ||
+    password1 === '' ||
+    email === '';
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input 
+        <input
           type='text'
           value={userName}
           onChange={(e) => setUserName(e.currentTarget.value)}
-          placeholder='Nombre de Usuario'/>
+          placeholder='Nombre de Usuario' />
 
-          <input 
+        <input
           type='text'
           value={email}
           onChange={(e) => setEmail(e.currentTarget.value)}
-          placeholder='Email'/>
+          placeholder='Email' />
 
-          <input 
+        <input
           type='text'
           value={password1}
           onChange={(e) => setPassword1(e.currentTarget.value)}
-          placeholder='Contraseña'/>
+          placeholder='Contraseña' />
 
-          <input 
+        <input
           type='text'
           value={password2}
           onChange={(e) => setPassword2(e.currentTarget.value)}
-          placeholder='Repita Contraseña'/>
+          placeholder='Repita Contraseña' />
 
-          <button type='submit' disabled={isInvalid}>Registrarse</button>
-          
-          {error && <p>{error.message}</p>}
+        <input
+          type='text'
+          value={departamento}
+          onChange={e => setDepartamento(e.currentTarget.value)}
+          placeholder='Ingrese Departamento' />
+
+
+        <button type='submit' disabled={isInvalid}>Registrarse</button>
+
+        {error && <p>{error.message}</p>}
       </form>
+      {departamento}
     </div>
   )
 }
